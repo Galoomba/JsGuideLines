52b90e5e2dcc25e660a7df2c9ae0515401896d3a
Galoomba <david.hany129@gmail.com>
2019-01-29 17:41:58 +0200

    Objects

--- JsGuideLines.js
+++ JsGuideLines.js
@@ -1,13 +1,16 @@
 /**
- * Using let & const insisted of var 
- * const -> the value gonna stay constant
- * let -> same as var but have different scope level
+ * using objects
  */
 
-const name='Daviid';
-let age =24;
-
-const levelUp=(name ,age)=> name+" "+ ++age;
-
-
-console.log(levelUp(name,age));
+const Person={
+    name:'Daviid',
+    age :24,
+    //functions shapes..
+        levelUp(name ,age){ return this.name+" "+ this.age++;},
+        levelUpUp:function(name ,age){return this.name+" "+ this.age++;},
+        //In Arrow function This keyword refere to the globle scope not the object scope
+        levelUpUpUp:(name ,age)=>{this.name+" "+ this.age++;}
+}
+console.log(Person.levelUp(Person.name,Person.age));
+console.log(Person.levelUpUp(Person.name,Person.age));
+console.log(Person.levelUpUpUp(Person.name,Person.age));
