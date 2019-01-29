d93d4923465024077cff315f5b2e382ed7253950
Galoomba <david.hany129@gmail.com>
2019-01-29 16:55:47 +0200

    var and function --old-school

---
+++ JsGuideLines.js
@@ -0,0 +1,12 @@
+/**
+ * declaring 2 variables , passing them to a function and calling it 
+ */
+
+var name='Daviid';
+var age =24;
+
+function print(name ,age){
+    return name+" "+age;
+}
+
+console.log();
