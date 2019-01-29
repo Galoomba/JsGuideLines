cfac9199d108b594a09063598f62caef8a42eaac
Galoomba <david.hany129@gmail.com>
2019-01-29 18:06:20 +0200

    Arrays Are Reference types

--- JsGuideLines.js
+++ JsGuideLines.js
@@ -4,11 +4,20 @@
 
 const hobbies = ['Sports','Cooking']
 
-//foreach loop 
-for (let hobby of hobbies)
-    console.log(hobby)
+// //foreach loop 
+// for (let hobby of hobbies)
+//     console.log(hobby)
 
-//using map function 
-console.log(hobbies.map(hobby=>"hoppy "+hobby))
-//orginal array stayed the same 
+// //using map function 
+// console.log(hobbies.map(hobby=>"hoppy "+hobby))
+// //orginal array stayed the same 
+
+
+/**
+ * Array Objects Reference types 
+ * --> Although it's a constant we can still add and delete element to it
+ * as the Reference is the constant not the element 
+ */
+hobbies.push('Fishing')
+
 console.log(hobbies)
